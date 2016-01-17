f14b8ff75e0a638c31885d3a5dd9efc642ad9bcf
Anton Linevich <linevich.net@gmail.com>
2016-01-17 20:39:13 +0200

    Translations updated

--- makemessages.sh
+++ makemessages.sh
@@ -2,5 +2,6 @@
 POTFILE=./locales/messages.pot
 LOCALES_PATH=locales/
 
+make potfile
 pybabel update --input-file $POTFILE --output-dir $LOCALES_PATH --domain messages
 #pybabel init --input-file $POTFILE --output-dir $LOCALES_PATH --locale "$1" --domain messages
